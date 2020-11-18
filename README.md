027a3cdf2328f5105681c0dda9b67e529b11f9b5
andreamandrici <andrea.mandrici@gmail.com>
2020-11-18 18:31:02 +0100

    updated on 20201118 at 18:31:02

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # dopa_rest
 
-Extract the whole archive.
-Edit file `02_populate_tables.sql`, and modify paths in the lines with keywords "locura,burmini,mazinga,grecia" (choose WIN or LINUX version, commenting the unwanted with "--").
+Extract the whole archive (be aware: is UTF8).
+
+Edit file `02_populate_tables.sql`, and modify paths in the blocks marked with keywords "locura,mazinga" (choose WIN or LINUX version, commenting the unwanted with "--").
+
 In PGAdmin, execute in sequence:
+
 1. `01_create_infrastructure.sql`, which creates
 	+ schema `mtz_results`
 	+ tables
 		+ `country`
 		+ `ecoregion_in_country`
-2.  `02_create_infrastructure.sql`, which creates
+2.  `02_create_infrastructure.sql`, which populates the above tables;
+
+3. execute alternatively: 03(a/b/c)_create_outputs.sql
+
+4. execute:
+	+ `SELECT * FROM mtz_results.country_all_inds;` (check the field ecoregion_in_country)
+	+ `SELECT * FROM mtz_results.get_country_all_inds();` -- check the field ecoregion_in_country
+	+ `SELECT * FROM mtz_results.get_country_ecoregion_stats();`
+
+5. discuss it together.
+
+A.	
+
       
---
+++ sql/03a_create_outputs.sql
@@ -0,0 +1,25 @@
+-- CLEANUP
+DROP TABLE IF EXISTS mtz_results.country_all_inds CASCADE;
+DROP TABLE IF EXISTS mtz_results.country_all_inds_template CASCADE;
+DROP TABLE IF EXISTS mtz_results.country_ecoregions_stats_template CASCADE;
+-- CREATE TABLE country_all_inds
+CREATE TABLE mtz_results.country_all_inds AS
+SELECT * FROM mtz_results.country
+LEFT JOIN (
+		SELECT country_id,ARRAY_AGG((b.*)) ecoregion_in_country
+		FROM mtz_results.ecoregion_in_country b
+		GROUP BY country_id ORDER BY country_id
+		) c
+USING(country_id)
+ORDER BY country_id;
+-- CREATE TEMPLATE country_all_inds_template
+CREATE TABLE mtz_results.country_all_inds_template AS SELECT * FROM mtz_results.country_all_inds LIMIT 0;
+-- CREATE FUNCTION get_country_all_inds
+CREATE FUNCTION mtz_results.get_country_all_inds()
+RETURNS SETOF mtz_results.country_all_inds_template LANGUAGE SQL AS $BODY$ SELECT * FROM mtz_results.country_all_inds; $BODY$;
+
+-- CREATE TEMPLATE country_ecoregions_stats_template
+CREATE TABLE mtz_results.country_ecoregions_stats_template AS SELECT (UNNEST(ecoregion_in_country)).* FROM mtz_results.country_all_inds LIMIT 0;
+-- CREATE FUNCTION get_country_ecoregions_stats
+CREATE FUNCTION mtz_results.get_country_ecoregion_stats()
+RETURNS SETOF mtz_results.country_ecoregions_stats_template LANGUAGE SQL AS $BODY$ SELECT (UNNEST(ecoregion_in_country)).* FROM mtz_results.country_all_inds; $BODY$;
